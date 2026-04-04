c9509422aa585de0ad47efc1a48be18890eddeb2
Pau Marín Roig <pmarinroig@gmail.com>
2026-04-04 14:35:59 +0200

    Default fabric mod backend to hosted URL

---
+++ packages/fabric/src/main/java/link/sharedworld/SharedWorldBackendConstants.java
@@ -0,0 +1,9 @@
+package link.sharedworld;
+
+final class SharedWorldBackendConstants {
+    static final String DEFAULT_BASE_URL = "https://sharedworld.farigola777.workers.dev";
+    static final String BACKEND_URL_SYSTEM_PROPERTY = "sharedworld.backendUrl";
+
+    private SharedWorldBackendConstants() {
+    }
+}
